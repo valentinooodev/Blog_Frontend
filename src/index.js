951d9a83d172d1dc271277c3b5e966425e200a66
= <=>
2021-11-30 14:30:38 +0700

    basic post list

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const routing = (
+    <BrowserRouter>
+        <React.StrictMode>
+            <Header/>
+            <Routes>
+                <Route exact path="/" element={<App/>}/>
+            </Routes>
+            <Footer/>
+        </React.StrictMode>
+    </BrowserRouter>
 );
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
